4fc8fcffae20bae34e1f85601a4ff4a03d52bf54
Mike De Groot <34241515+mdegroot09@users.noreply.github.com>
2021-07-08 17:25:10 -0600

    added error notification

--- EarlyAccessRequest.js
+++ EarlyAccessRequest.js
@@ -1,11 +1,10 @@
 function pickEarlyAccessRequest(){
     var touringAgent = parent.Xrm.Page.data.entity.attributes.get('homie_touringagent').getValue();
+    var systemuserid = parent.Xrm.Page.context.getUserId();
 
     // if Touring Agent on Early Access Request is blank on form instance
     if (!touringAgent){
-        var activityid = parent.Xrm.Page.data.entity.attributes.get('activityid').getValue();
-        var systemuserid = parent.Xrm.Page.context.getUserId();
-
+        var activityid = parent.Xrm.Page.data.entity.getId()
         var req = new XMLHttpRequest();
         var url = "https://prod-28.westus.logic.azure.com:443/workflows/21a1726b7cb54ff38cfeb7153031011b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=cPW0erHTq-WNpoLYt01SVMLFdFvoMI5Vov-V721ifEE";
         req.open("PUT", url, true);
@@ -14,5 +13,14 @@ function pickEarlyAccessRequest(){
             "activityid": activityid,
             "systemuserid": systemuserid
         }));
+    }
+
+    // Display an error that a Touring Agent is already assigned
+    else {
+        var messageId = `${systemuserid}${Date.now()}`
+        Xrm.Page.ui.setFormNotification("A Touring Agent has already been assigned.", "ERROR", messageId);
+        setTimeout(() => {
+            Xrm.Page.ui.clearFormNotification(messageId)
+        }, 5000);
     }
 }
