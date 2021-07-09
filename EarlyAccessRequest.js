a2ba78512f5df32c4f0e5fa62c80cd988ad06497
Mike De Groot <34241515+mdegroot09@users.noreply.github.com>
2021-07-09 14:53:53 -0600

    major refactor, added notifications

--- EarlyAccessRequest.js
+++ EarlyAccessRequest.js
@@ -1,26 +1,82 @@
 function pickEarlyAccessRequest(){
     var touringAgent = parent.Xrm.Page.data.entity.attributes.get('homie_touringagent').getValue();
     var systemuserid = parent.Xrm.Page.context.getUserId();
+    var activityid = parent.Xrm.Page.data.entity.getId();
 
     // if Touring Agent on Early Access Request is blank on form instance
     if (!touringAgent){
-        var activityid = parent.Xrm.Page.data.entity.getId()
-        var req = new XMLHttpRequest();
-        var url = "https://prod-28.westus.logic.azure.com:443/workflows/21a1726b7cb54ff38cfeb7153031011b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=cPW0erHTq-WNpoLYt01SVMLFdFvoMI5Vov-V721ifEE";
-        req.open("PUT", url, true);
-        req.setRequestHeader('Content-Type', 'application/json');
-        req.send(JSON.stringify({
-            "activityid": activityid,
-            "systemuserid": systemuserid
-        }));
+        triggerFlowToAddTouringAgent(systemuserid, activityid)
     }
 
     // Display an error that a Touring Agent is already assigned
     else {
-        var messageId = `${systemuserid}${Date.now()}`
-        Xrm.Page.ui.setFormNotification("A Touring Agent has already been assigned.", "ERROR", messageId);
-        setTimeout(() => {
-            Xrm.Page.ui.clearFormNotification(messageId)
-        }, 5000);
+        setFailureNotification()
     }
+}
+
+function triggerFlowToAddTouringAgent(systemuserid, activityid){
+    try {
+        var req = new XMLHttpRequest();
+        var url = "https://prod-28.westus.logic.azure.com:443/workflows/21a1726b7cb54ff38cfeb7153031011b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=cPW0erHTq-WNpoLYt01SVMLFdFvoMI5Vov-V721ifEE";
+        req.open("PUT", url, true);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.onreadystatechange = function (){
+            if (this.readyState == 4 /* complete */ || this.readyState == 2){
+                req.onreadystatechange = null;
+                if (this.status == 200 || this.status == 204 || this.status == 202){
+                    // success
+                    setSuccessNotification()
+                }
+                else {
+                    // error 
+                    setFailureNotification()
+                }
+            }
+        }
+        req.send(JSON.stringify({
+            "systemuserid": systemuserid,
+            "activityid": activityid
+        }))
+    }
+    catch (e){
+        // error 
+        setFailureNotification()
+    }
+}
+
+function setSuccessNotification(){
+    let notification = {
+        message: "Success",
+        level: 1,
+        showCloseButton: true,
+        type: 2
+    }
+    Xrm.App.addGlobalNotification(notification).then(
+        function success(messageId){
+            clearNotification(messageId, "Success")
+        }, function (err){} // error callback
+    )
+}
+
+function setFailureNotification(){
+    let message = "A Touring Agent has already been assigned."
+    let systemuserid = parent.Xrm.Page.context.getUserId();
+    let messageId = `${systemuserid}${Date.now()}`
+    Xrm.Page.ui.setFormNotification(message, "ERROR", messageId);
+    clearNotification(messageId, "Error")
+}
+
+function clearNotification(messageId, type){
+    // refresh the page
+    Xrm.Page.data.refresh()
+
+    setTimeout(() => {
+        if (type == "Success"){
+            // clear success notification
+            Xrm.App.clearGlobalNotification(messageId)
+        } else { 
+            // clear error notification
+            Xrm.Page.ui.clearFormNotification(messageId)
+        }
+    }, 5000);
 }
